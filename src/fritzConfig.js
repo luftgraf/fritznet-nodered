bf3cb0ac91692d5452fc2191eda209837f8937a6
luftgraf <122802376+luftgraf@users.noreply.github.com>
2023-01-16 15:12:20 +0100

    Update fritzConfig.js

--- src/fritzConfig.js
+++ src/fritzConfig.js
@@ -3,6 +3,7 @@ module.exports = function(RED) {
         RED.nodes.createNode(this, config);
         this.username = config.username;
         this.password = config.password;
+        this.boxurl = config.boxurl;
     }
     RED.nodes.registerType("fritz-box", fritzBox);
 }
